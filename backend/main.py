1f3d1db808c1dab62ece4ac91f1ea7d5185d0928
SAHILKUMARNEHRA <sk.nehra2005@gmail.com>
2026-03-28 15:39:01 +0530

    feat: build FastAPI endpoints in main.py

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,10 @@ from typing import List
 import os
 
 from database import engine, get_db, Base
+import models
+from services.pdf_parser import extract_text_from_pdf, chunk_text
+from services.embedding import get_embeddings_batch, get_embedding
+from services.ai_chat import get_answer
+
+# Initialize Database
+models.Base.metadata.create_all(bind=engine)
